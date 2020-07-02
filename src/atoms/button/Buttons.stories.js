28deabc245b2db9aeba1f50ef041529c80aaeea8
ComeAlongErica <ericaraephoto@gmail.com>
2020-07-02 18:32:13 -0400

    add: ghost story

--- src/atoms/button/Buttons.stories.js
+++ src/atoms/button/Buttons.stories.js
@@ -5,7 +5,7 @@ import Button from '../../../components/atoms/Button'
 
 export default { title: 'Button' }
 
-export const withText = () => {
+export const primaryButton = () => {
   const buttonClick = () => console.log('clicked')
   return (
     <Button
@@ -13,7 +13,22 @@ export const withText = () => {
       onClick={action('button-click')}
       loading={boolean('loading', false)}
       disabled={boolean('disabled', false)}
-      variant={text('variant', 'Primary')}
+      variant={text('variant', 'primary')}
+    >
+      {text('children', 'Click Me')}
+    </Button>
+  )
+}
+
+export const ghostButton = () => {
+  const buttonClick = () => console.log('clicked')
+  return (
+    <Button
+      styledComp={text('styledComp', 'width: 150px; height: 45px;')}
+      onClick={action('button-click')}
+      loading={boolean('loading', false)}
+      disabled={boolean('disabled', false)}
+      variant={text('variant', 'ghost')}
     >
       {text('children', 'Click Me')}
     </Button>

e99d1b181f1948c0dde7946311b1b5ac7af21cbc
ComeAlongErica <ericaraephoto@gmail.com>
2020-07-18 09:58:27 -0400

    remove: knobs from main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   stories: ['../src/**/*.stories.[tj]s'],
   addons: [
     '@storybook/addon-actions',
-    '@storybook/addon-knobs',
     '@storybook/addon-docs/react/preset'
   ]
 }
